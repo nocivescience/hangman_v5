3d784e0c3f2582bc785882ff15e6caf2faba8f1c
noceive <nocivescience@gmail.com>
2022-10-26 20:07:28 -0300

    empezando desde cero

--- script.js
+++ script.js
@@ -7,45 +7,4 @@ const messageEl=document.getElementById('messageContainer');
 const botonPlay=document.getElementById('botonPlay');
 const correctLetters=[];
 const wrongLetters=[];
-function creatingCards(){
-    tarjetasContainer.innerHTML=`
-        ${
-            selectedWord
-            .split('')
-            .map((letter,i)=>`
-                <span class="letter" id="letter-${i}">
-                    ${correctLetters.includes(letter)?letter:''}
-                </span>
-            `)
-            .join('')
-        }
-    `;
-    const innerWord=tarjetasContainer.innerText.replace(/\n/g,'');
-};
-function updateFromLettersEl(){
-    wrongLettersEl.innerHTML=`
-        ${wrongLetters.length}
-    `;
-    const message=undefined;
-    messageEl.innerHTML=`
-        ${wrongLetters.length>0?'<p>Errado<p>':'Excelente'}
-    `;
-}
-updateFromLettersEl();
-document.addEventListener('keydown',(e)=>{
-    if(e.keyCode>=65 && e.keyCode<=90){
-        const letter=e.key;
-        if(selectedWord.includes(letter)){
-            if(!correctLetters.includes(letter)){
-                correctLetters.push(letter);
-                creatingCards();
-            }
-        }
-    }
-})
-botonPlay.addEventListener('click',()=>{
-    correctLetters.splice(0);
-    wrongLetters.splice(0);
-    selectedWord=listaPalabras[Math.floor(Math.random()*listaPalabras.length)];
-});
-creatingCards();
+let listaSeparada=[];
